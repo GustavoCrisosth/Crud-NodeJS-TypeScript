36eb55842eb6c142008027a9b9b158e58428cf7e
Gustavo Crisosth <favocrisosth5@gmail.com>
2025-10-29 10:42:18 -0300

    Configura frontend para URL de API de produção

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,8 +1,10 @@
 
 import axios from 'axios';
 
+const apiURL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: apiURL,
 });
 
 export default api;
